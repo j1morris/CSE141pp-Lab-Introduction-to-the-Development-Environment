1e9b2cd04cbb8881aa861ff4ec55f82e3f6fa3e4
Justin Morris <j1morris@ucsd.edu>
2019-08-05 15:15:45 -0700

    new lab.py

--- lab.py
+++ lab.py
@@ -8,7 +8,7 @@ input_files = ['submission/code.c']
 run_cmd = ['make', '-C', 'submission']
 clean_cmd = ['make', 'clean']
 env = ['CC','CXX','CFLAGS','CXXFLAGS']
-repo = "https://github.com/NVSL/CSE141pp-Lab-Introduction-to-the-Development-Environment/edit/master/lab.py"
+repo = "https://github.com/NVSL/CSE141pp-Lab-Introduction-to-the-Development-Environment"
 reference_tag = "314bfbd09ab3a28b446742234851eeef2c29dcba"
 time_limit = 40
 
